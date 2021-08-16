8efa559c2476128cf870003f248dc7f18b57c04d
nitheeshmashraf <nitheeshmashraf@gmail.com>
2021-08-16 21:10:45 +0530

    import views

--- saleor/urls.py
+++ saleor/urls.py
@@ -12,7 +12,7 @@ from .plugins.views import (
     handle_plugin_webhook,
 )
 from .product.views import digital_product
-
+from .core import views
 urlpatterns = [
     url(r"^graphql/$", csrf_exempt(GraphQLView.as_view(schema=schema)), name="api"),
     url(r"confirm_mail/", views.confirm_mail),
